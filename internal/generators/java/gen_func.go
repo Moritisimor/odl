4d94016b3ffde923dbc64c212c006441034140ff
Moritisimor <devMoritisimor@proton.me>
2026-05-31 12:06:12 +0200

    Java Generator now generates all args constructor

--- internal/generators/java/gen_func.go
+++ internal/generators/java/gen_func.go
@@ -36,11 +36,11 @@ func GenerateJava(objDefs []models.ObjectDefinition) (map[string]string, error) 
 				return files, fmt.Errorf("i could not find a java equivalent to the type '%s'", f.FieldType)
 			}
 
-			builder.WriteString("\tprivate ")
-			builder.WriteString(javaType)
-			builder.WriteString(" ")
-			builder.WriteString(helpers.CamelCase(f.Name))
-			builder.WriteString(";\n")
+			fmt.Fprintf(
+				&builder,
+				"\tprivate %s %s;\n",
+				javaType, helpers.CamelCase(f.Name),
+			)
 		}
 
 		// Getters
@@ -52,13 +52,13 @@ func GenerateJava(objDefs []models.ObjectDefinition) (map[string]string, error) 
 
 			if javaType == "boolean" && f.Name[0] == "is" {
 				fmt.Fprintf(
-					&builder, 
+					&builder,
 					"\n\tpublic boolean %s() {\n\t\treturn this.%s;\n\t}\n",
 					helpers.CamelCase(f.Name), helpers.CamelCase(f.Name),
 				)
 			} else {
 				fmt.Fprintf(
-					&builder, 
+					&builder,
 					"\n\tpublic %s get%s() {\n\t\treturn this.%s;\n\t}\n",
 					javaType, helpers.PascalCase(f.Name), helpers.CamelCase(f.Name),
 				)
@@ -83,7 +83,43 @@ func GenerateJava(objDefs []models.ObjectDefinition) (map[string]string, error) 
 			)
 		}
 
-		builder.WriteString("}\n")
+		// Constructor
+		fmt.Fprintf(
+			&builder,
+			"\n\tpublic %s(\n",
+			helpers.PascalCase(obj.Name),
+		)
+
+		for i, f := range obj.Fields {
+			javaType, ok := javaTypes[f.FieldType]
+			if !ok {
+				return files, fmt.Errorf("i could not find a java equivalent to the type '%s'", f.FieldType)
+			}
+
+			fmt.Fprintf(
+				&builder,
+				"\t\t%s %s",
+				javaType, helpers.CamelCase(f.Name),
+			)
+
+			if i < len(obj.Fields)-1 {
+				builder.WriteString(",")
+			}
+
+			builder.WriteString("\n")
+		}
+
+		builder.WriteString("\t) {\n")
+		for _, f := range obj.Fields {
+			fmt.Fprintf(
+				&builder,
+				"\t\tthis.%s = %s;\n",
+				helpers.CamelCase(f.Name),
+				helpers.CamelCase(f.Name),
+			)
+		}
+
+		builder.WriteString("\t}\n}\n")
 		files[helpers.PascalCase(obj.Name)] = builder.String()
 	}
 
