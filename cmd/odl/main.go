3f14f8815eabf374de0e4bbdf99fe34cedff539b
Moritisimor <devMoritisimor@proton.me>
2026-05-31 09:48:14 +0200

    Implemented CLI mode for main function

--- cmd/odl/main.go
+++ cmd/odl/main.go
@@ -11,12 +11,15 @@ import (
 
 func main() {
 	args := os.Args
-	if len(args) < 2 {
-		fmt.Printf("Usage: odl <file_name>\n")
+	if len(args) < 3 {
+		fmt.Printf("Usage: odl <file_name> <transpiler_target>\n")
 		os.Exit(1)
 	}
 
-	data, err := os.ReadFile(args[1])
+	fileName := args[1]
+	transpilerTarget := args[2]
+
+	data, err := os.ReadFile(fileName)
 	if err != nil {
 		fmt.Printf("Error while reading '%s': %s\n", args[1], err.Error())
 		os.Exit(1)
@@ -36,8 +39,35 @@ func main() {
 		os.Exit(1)
 	}
 
-	javaFiles, err := java.GenerateJava(objs)
-	for fileName, fileContent := range javaFiles {
-		fmt.Printf("==( %s )==\n%s", fileName, fileContent)
+	var files map[string]string
+	switch transpilerTarget {
+	case "java":
+		files, err = java.GenerateJava(objs)
+
+	default:
+		fmt.Printf("Unknown transpiler target '%s'\n", transpilerTarget)
+		os.Exit(1)
+	}
+
+	if err != nil {
+		fmt.Printf("Error while generating %s source code: %s", transpilerTarget, err.Error())
+		os.Exit(1)
+	}
+
+	for className, fileContent := range files {
+		var fileName string
+		switch transpilerTarget {
+		case "java":
+			fileName = className + ".java"
+
+		default:
+			fileName = className
+		}
+
+		err := os.WriteFile(fileName, []byte(fileContent), 0755)
+		if err != nil {
+			fmt.Printf("Could not write generator output to '%s': %s\n", fileName, err.Error())
+			continue
+		}
 	}
 }
