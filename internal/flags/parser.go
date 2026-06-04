1994a5edfbe443dd7b173cca7caedc31aac7da1f
Moritisimor <devMoritisimor@proton.me>
2026-06-04 16:56:37 +0200

    Flag parser fails on unknown flags now (again)

--- internal/flags/parser.go
+++ internal/flags/parser.go
@@ -29,6 +29,7 @@ func ParseFlags(args []string) (Flags, error) {
 			}
 
 			flags.Input = arg
+			continue
 		}
 
 		switch arg {
@@ -47,6 +48,9 @@ func ParseFlags(args []string) (Flags, error) {
 
 			flags.Output = args[idx + 1]
 			ignoreable = append(ignoreable, idx + 1)
+
+		default:
+			return flags, fmt.Errorf("Unknown flag: '%s'", arg)
 		}
 	}
 
